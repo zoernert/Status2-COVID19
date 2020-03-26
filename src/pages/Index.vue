ee736797178527b6f9f7bb226a992b0c80e18f40
Thorsten Zoerner <me@thorsten-zoerner.com>
2020-03-26 01:02:19 +0100

    Fixed Push Notification Bug

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -249,7 +249,7 @@ export default {
       const OneSignal = window.OneSignal
       const parent = this
       OneSignal.push(['getNotificationPermission', function (permission) {
-        OneSignal.push(['sendTags', { code: window.localString('code'), domain: location.hostname, availability: this.availability }])
+        OneSignal.push(['sendTags', { code: window.localStorage.getItem('code'), domain: location.hostname, availability: this.availability }])
         if (permission === 'granted') {
 
         } else {
