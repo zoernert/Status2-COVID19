92527391664b8de27701d7273d7159b42d56663c
Thorsten Zoerner <me@thorsten-zoerner.com>
2020-03-31 00:44:49 +0200

    AutoDeploy

--- quasar.conf.js
+++ quasar.conf.js
@@ -55,7 +55,9 @@ module.exports = function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-
+      env: {
+        BRANCH: JSON.stringify(process.env.BRANCH)
+      }
       // rtl: false, // https://quasar.dev/options/rtl-support
       // showProgress: false,
       // gzip: true,
