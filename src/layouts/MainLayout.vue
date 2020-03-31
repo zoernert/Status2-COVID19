0f90e01ff768635af5e5fdd4522a99cb2369c5f3
Thorsten Zoerner <me@thorsten-zoerner.com>
2020-04-01 00:01:18 +0200

    AutoDeploy

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -15,7 +15,7 @@
           Status2
         </q-toolbar-title>
 
-        <div><q-avatar><img src='/statics/app-icon.png'/></q-avatar></div>
+        <div><a href='https://www.startnext.com/status2/' target='_blank'>Info</a></div>
       </q-toolbar>
     </q-header>
 
